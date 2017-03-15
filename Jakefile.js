d455b2a0e2919e25b5ba9ba6425fe6d8903bf1ba
canek <sknest@gmail.com>
2017-03-15 12:42:58 +0100

    Instalirao JSHint

--- Jakefile.js
+++ Jakefile.js
@@ -2,9 +2,24 @@
 (function () {
     "use strict";
 
+    var semver = require("semver");
+
     desc("Default build");
-    task("default", function () {
+    task("default", [ "version" ],  function () {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version");
+    task("version", function () {
+        console.log("Checking Node version: .");
+
+        var packageJson = require("./package.json");
+        var expectedVersion = packageJson.engines.node;
+
+        var actualVersion = process.version;
+        if(semver.neq(expectedVersion, actualVersion)){
+            fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion);
+        }
     });
 
 }());
--- jake.cmd
+++ jake.cmd
@@ -1,3 +1,8 @@
 @echo off
 
-node_modules\.bin\jake %*
+if exist node_modules\.bin\jake goto :run_jake
+echo Building npm modules:
+call npm rebuild
+
+:run_jake
+call node_modules\.bin\jake %*
